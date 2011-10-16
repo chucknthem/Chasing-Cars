6a6d4efef9bb97aa80868c4b1e239c0c569b2042
Charles Ma <chuckjma@gmail.com>
2011-10-15 17:31:42 -0700

    progress, got touch working

--- src/com/hackathon/chasingcars/Game.java
+++ src/com/hackathon/chasingcars/Game.java
@@ -1,9 +1,16 @@
 package com.hackathon.chasingcars;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import com.hackathon.chasingcars.entity.Player;
 import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.camera.SmoothCamera;
+import org.anddev.andengine.engine.handler.IUpdateHandler;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
@@ -11,7 +18,9 @@ import org.anddev.andengine.entity.primitive.Line;
 import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.input.touch.TouchEvent;
 import org.anddev.andengine.opengl.texture.TextureManager;
 
 /**
@@ -21,7 +30,7 @@ import org.anddev.andengine.opengl.texture.TextureManager;
  * @author Nicolas Gramlich
  * @since 11:54:51 - 03.04.2010
  */
-public class Game extends BaseChasingCarActivity {
+public class Game extends BaseChasingCarActivity implements Scene.IOnSceneTouchListener, IUpdateHandler {
     // ===========================================================
     // Constants
     // ===========================================================
@@ -29,17 +38,24 @@ public class Game extends BaseChasingCarActivity {
     /* Initializing the Random generator produces a comparable result over different versions. */
     private static final long RANDOM_SEED = 1234567890;
 
-    private static final int MAP_WIDTH = 5000;
-    private static final int MAP_HEIGHT = 5000;
-    
-    private static final int CAMERA_WIDTH = 1000;
-    private static final int CAMERA_HEIGHT = 800;
+    private static final float MAP_WIDTH = 5000;
+    private static final float MAP_HEIGHT = 5000;
 
+    private static final float STARTX = MAP_WIDTH/2;
+    private static final float STARTY = MAP_HEIGHT/2;
+
+    private static final float CAMERA_WIDTH = 1000;
+    private static final float CAMERA_HEIGHT = 800;
+    private static final float CAM_MAX_VELOCITY_X = 200.0f;
+    private static final float CAM_MAX_VELOCITY_Y = 200.0f;
+    private static final float CAM_ZOOM_FACTOR = 5f;
+    
     private static final int LINE_COUNT = 100;
 
     private static final int BG_RED = 1;
     private static final int BG_GREEN = 1;
     private static final int BG_BLUE = 1;
+
 
 
     // ===========================================================
@@ -48,7 +64,16 @@ public class Game extends BaseChasingCarActivity {
 
     private static TextureManager mTextureManager;
 
-    private Camera mCamera;
+    private SmoothCamera mCamera;
+
+    private List<Player> mPlayers = new LinkedList<Player>();
+
+    private Player mThisPlayer;
+
+    private boolean mIsAccelerating = false;
+
+    // Touch
+    private float mTouchX, mTouchY, mTouchOffsetX, mTouchOffsetY;
 
     // ===========================================================
     // Constructors
@@ -64,7 +89,7 @@ public class Game extends BaseChasingCarActivity {
 
     @Override
     public Engine onLoadEngine() {
-        this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        this.mCamera = new SmoothCamera(STARTX, STARTY, CAMERA_WIDTH, CAMERA_HEIGHT, CAM_MAX_VELOCITY_X, CAM_MAX_VELOCITY_Y, CAM_ZOOM_FACTOR);
         this.mTextureManager = new TextureManager();
         
         return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(MAP_WIDTH, MAP_HEIGHT), this.mCamera));
@@ -78,36 +103,50 @@ public class Game extends BaseChasingCarActivity {
     @Override
     public Scene onLoadScene() {
         this.mEngine.registerUpdateHandler(new FPSLogger());
-
+        this.mCamera.setCenter(STARTX, STARTY);
+        //this.mEngine.registerUpdateHandler(this);
+        
         final Scene scene = new Scene();
         scene.setBackground(new ColorBackground(BG_RED, BG_GREEN, BG_BLUE));
 
-        final Random random = new Random(RANDOM_SEED);
+        mThisPlayer = new Player("bob", "1", STARTX, STARTY);
+        mThisPlayer.setColor(Player.COLOR.RED);
 
-        for(int i = 0; i < LINE_COUNT; i++) {
-            final float x1 = random.nextFloat() * CAMERA_WIDTH;
-            final float x2 = random.nextFloat() * CAMERA_WIDTH;
-            final float y1 = random.nextFloat() * CAMERA_HEIGHT;
-            final float y2 = random.nextFloat() * CAMERA_HEIGHT;
-            final float lineWidth = random.nextFloat() * 5;
+        scene.attachChild(mThisPlayer);
 
-            final Player rec = new Player("bob", "1", x1, y1);
+        mPlayers.add(mThisPlayer);
 
-            rec.setColor(random.nextFloat(), random.nextFloat(), random.nextFloat());
+        scene.registerUpdateHandler(this);
+        // Register activity to handle touch events.
+        scene.setOnSceneTouchListener(this);
 
-            scene.attachChild(rec);
-        }
-
-        Player player = new Player("bob", "1", 0, 0);
-        //Rectangle player = new Rectangle(0, 0, 10, 10);
-        player.setColor(1.0f, 0.0f, 0.0f);
-        scene.attachChild(player);
         return scene;
     }
 
     @Override
     public void onLoadComplete() {
 
+    }
+
+    /**
+     * Called when a {@link org.anddev.andengine.input.touch.TouchEvent} is dispatched to a {@link org.anddev.andengine.entity.scene.Scene}.
+     *
+     * @param pScene           The {@link org.anddev.andengine.entity.scene.Scene} that the {@link org.anddev.andengine.input.touch.TouchEvent} has been dispatched to.
+     * @param pSceneTouchEvent The {@link org.anddev.andengine.input.touch.TouchEvent} object containing full information about the event.
+     * @return <code>true</code> if this {@link org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener} has consumed the {@link org.anddev.andengine.input.touch.TouchEvent}, <code>false</code> otherwise.
+     */
+    @Override
+    public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+        int action = pSceneTouchEvent.getAction();
+
+        if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE) {
+            this.mIsAccelerating = true;
+            this.mTouchX = pSceneTouchEvent.getMotionEvent().getX();
+            this.mTouchY = pSceneTouchEvent.getMotionEvent().getY();
+        } else if (action == MotionEvent.ACTION_UP) {
+            this.mIsAccelerating = false;
+        }
+        return true;
     }
 
     // ===========================================================
@@ -117,4 +156,21 @@ public class Game extends BaseChasingCarActivity {
     // ===========================================================
     // Inner and Anonymous Classes
     // ===========================================================
+
+    @Override
+    public void onUpdate(float pSecondsElapsed) {
+        if (mIsAccelerating) {
+            float aX = mThisPlayer.getX() - this.mCamera.getMinX() - mTouchX;
+            float aY = mThisPlayer.getY() - this.mCamera.getMinY() - mTouchY;
+            Log.w("Touch", mTouchX + " " + mTouchY);
+            Log.w("Player", mThisPlayer.getX() + " " + mThisPlayer.getY());
+
+            mThisPlayer.accelerate(aX, aY);
+        }
+    }
+
+    @Override
+    public void reset() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
--- src/com/hackathon/chasingcars/entity/Player.java
+++ src/com/hackathon/chasingcars/entity/Player.java
@@ -1,5 +1,7 @@
 package com.hackathon.chasingcars.entity;
 
+import android.util.Log;
+import org.anddev.andengine.engine.handler.IUpdateHandler;
 import org.anddev.andengine.entity.primitive.Rectangle;
 import org.apache.http.client.RedirectException;
 
@@ -10,10 +12,14 @@ import org.apache.http.client.RedirectException;
  * Time: 2:06 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Player extends Rectangle {
+public class Player extends Rectangle implements IUpdateHandler {
 
     private static final float PLAYER_WIDTH = 40f;
     private static final float PLAYER_HEIGHT = 40f;
+
+    private static final float PLAYER_DECELERATION = 100f; // per second.
+    private static final float PLAYER_ACCELERATION = .1f;
+    
     public static enum COLOR {
         RED,
         BLUE,
@@ -24,6 +30,9 @@ public class Player extends Rectangle {
 
     private String name;
     private String id;
+   
+    private double mDirection = 0; // Radians.
+    private float mVelocity = 0;
 
     public Player(String name, String id,  final float startX, final float startY) {
         super(startX, startY, PLAYER_WIDTH, PLAYER_HEIGHT);
@@ -54,5 +63,26 @@ public class Player extends Rectangle {
                 break;
         }
         this.setColor(red, green, blue);
+    }
+
+    @Override
+    protected void onManagedUpdate(final float pSecondsElapsed) {
+        float moveX = mVelocity * pSecondsElapsed * (float) Math.cos(mDirection);
+        float moveY = mVelocity * pSecondsElapsed * (float) Math.sin(mDirection);
+        this.mX += moveX;
+        this.mY += moveY;
+
+        mVelocity = mVelocity - PLAYER_DECELERATION * pSecondsElapsed;
+        if (mVelocity < 0) mVelocity = 0;
+        
+        super.onManagedUpdate(pSecondsElapsed);
+    }
+
+    public void accelerate(final float x, final float y) {
+        float newX = mVelocity * (float) Math.cos(mDirection) + x * PLAYER_ACCELERATION;
+        float newY = mVelocity * (float) Math.sin(mDirection) + y * PLAYER_ACCELERATION;
+
+        mVelocity = (float) Math.sqrt((double)(newX * newX + newY * newY));
+        mDirection = Math.atan2((double)newX, (double) newY);
     }
 }
